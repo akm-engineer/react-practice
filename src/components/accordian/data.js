f3bbd96de8d5859d8ca3011fd0e61db084d9648e
Ashish Mishra <ashishgk1999@gmail.com>
2024-01-09 22:06:02 +0530

    Setup

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris",
+  },
+  {
+    id: 2,
+    question: "Who is the author of 'To Kill a Mockingbird'?",
+    answer: "Harper Lee",
+  },
+  {
+    id: 3,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+  },
+  {
+    id: 4,
+    question: "In which year did the Titanic sink?",
+    answer: "1912",
+  },
+];
+
+export default data;
---
+++ src/components/accordian/index.js
@@ -0,0 +1,3 @@
+export default function Accordian() {
+  return <div className="text-xl font-semibold">Ashish</div>;
+}
