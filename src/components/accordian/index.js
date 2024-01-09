d3c662f8e42a78082497063759a836887b681c48
Ashish Mishra <ashishgk1999@gmail.com>
2024-01-09 22:47:35 +0530

    accordian

--- src/components/accordian/index.js
+++ src/components/accordian/index.js
@@ -1,3 +1,40 @@
+import { useState } from "react";
+import data from "./data";
+
 export default function Accordian() {
-  return <div className="text-xl font-semibold">Ashish</div>;
+  const [selected, setSelected] = useState(null);
+  const handleClick = (currentId) => {
+    setSelected(currentId === selected ? null : currentId);
+  };
+  return (
+    <div className="text-xl font-semibold">
+      <button className="bg-green-700 rounded-lg hover:opacity-95 p-3 my-4">
+        Enable
+      </button>
+      <div>
+        {data && data.length > 0 ? (
+          data.map((item) => (
+            <div className="my-5 max-w-[450px] mx-auto ">
+              <div
+                className="bg-slate-700 rounded-lg gap-8 flex p-3 items-center justify-center"
+                onClick={() => handleClick(item.id)}
+              >
+                <h3 className="text-white">{item.question}</h3>
+                <span className="text-white text-lg cursor-pointer">+</span>
+              </div>
+              {selected === item.id ? (
+                <div>
+                  <p className="text-white bg-slate-400 rounded-lg p-3 my-2">
+                    {item.answer}
+                  </p>
+                </div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <h1 className="text-red-500 font-semibold">No data present</h1>
+        )}
+      </div>
+    </div>
+  );
 }
